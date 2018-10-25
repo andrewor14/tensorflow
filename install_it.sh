3a2185532b17f604b6653401113b25388a0b13c8
andrewor14 <andrewor14@gmail.com>
2018-10-25 00:38:17 -0400

    Add a helper install script

---
+++ install_it.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# WARNING: Do not run this while running tensorflow, or you may run into bus errors!
+
+TF_PKG_DIR="/home/andrewor/tensorflow_pkg"
+TF_PKG_NAME="tensorflow-1.10.1-cp36-cp36m-linux_x86_64.whl"
+
+module load anaconda3/5.2.0
+
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package "$TF_PKG_DIR"
+
+pip uninstall -y tensorflow tensorflow-gpu
+pip install --user "$TF_PKG_DIR/$TF_PKG_NAME"
+
