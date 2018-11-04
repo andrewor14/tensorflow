5a939c97a5b13e1780edc8e36eb5171f646f8038
andrewor14 <andrewor14@gmail.com>
2018-11-04 16:38:52 -0500

    Fix package path for version 1.12

--- install_it.sh
+++ install_it.sh
@@ -3,7 +3,7 @@
 # WARNING: Do not run this while running tensorflow, or you may run into bus errors!
 
 TF_PKG_DIR="/home/andrewor/tensorflow_pkg"
-TF_PKG_NAME="tensorflow-1.12.1-cp36-cp36m-linux_x86_64.whl"
+TF_PKG_NAME="tensorflow-1.12.0rc1-cp36-cp36m-linux_x86_64.whl"
 
 module load anaconda3/5.2.0
 
