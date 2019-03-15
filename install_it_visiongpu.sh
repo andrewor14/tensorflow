9682aa43500960e75132b4e4f0afb73d617a4f37
Andrew Or <andrewor14@gmail.com>
2019-03-15 15:24:32 -0400

    Add install script for visiongpu

--- install_it_visiongpu.sh
+++ install_it_visiongpu.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+# Note: This script must be run in virtual env!
 # WARNING: Do not run this while running tensorflow, or you may run into bus errors!
-
-TF_PKG_DIR="/home/andrewor/tensorflow_pkg"
-module load anaconda3/5.2.0
+TF_PKG_DIR="/home/andrewor/workspace/tensorflow_pkg"
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package "$TF_PKG_DIR"
 
 TF_PKG_NAME="$(ls $TF_PKG_DIR | grep .whl)"
