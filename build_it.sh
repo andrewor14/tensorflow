18a32e8c869cc4fb5a35ca76fb4c9524c720383a
Andrew Or <andrewor14@gmail.com>
2019-04-02 13:39:47 -0400

    Refactor build and install scripts (merge some files)

--- build_it.sh
+++ build_it.sh
@@ -1,43 +1,14 @@
 #!/bin/bash
 
-# Note: Do NOT use Anaconda 5.3.0, which uses Python 3.7, otherwise you'll
-# run into this issue https://github.com/tensorflow/tensorflow/pull/21202 
-module load anaconda3/5.2.0
-module load openmpi/gcc/3.0.0/64
-module load cudnn/cuda-9.2/7.3.1
-
-# Configure flags
-export PYTHON_BIN_PATH="/usr/licensed/anaconda3/5.2.0/bin/python"
-export PYTHON_LIB_PATH="/usr/licensed/anaconda3/5.2.0/lib/python3.6/site-packages"
-export TF_NEED_JEMALLOC=1
-export TF_NEED_GCP=1
-export TF_NEED_HDFS=1
-export TF_NEED_AWS=1
-export TF_NEED_KAFKA=1
-export TF_NEED_IGNITE=1
-export TF_ENABLE_XLA=1
-export TF_NEED_GDR=0
-export TF_NEED_VERBS=0
-export TF_NEED_OPENCL_SYCL=0
-export TF_NEED_ROCM=0
-export TF_NEED_CUDA=1
-export TF_CUDA_VERSION="9.2"
-export CUDA_TOOLKIT_PATH="/home/andrewor/lib/cuda-9.2"
-export TF_CUDNN_VERSION="7.3.1"
-export CUDNN_INSTALL_PATH="/home/andrewor/lib/cudnn-7.3.1"
-export TF_NEED_TENSORRT=0
-export TF_NCCL_VERSION="1.3"
-export TF_CUDA_COMPUTE_CAPABILITIES="3.5,3.7"
-export TF_CUDA_CLANG=0
-export GCC_HOST_COMPILER_PATH="/usr/bin/gcc"
-export TF_NEED_MPI=1
-export MPI_HOME="/home/andrewor/lib/openmpi"
-export CC_OPT_FLAGS="-march=native"
-export TF_SET_ANDROID_WORKSPACE=0
+source build_flags.sh
 
 ./configure
 
 echo "Done configuring. Now running bazel build."
 
-time bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+if [[ "$TF_NEED_CUDA" == "1" ]]; then
+  time bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+else
+  time bazel build -c opt //tensorflow/tools/pip_package:build_pip_package
+fi
 
--- install_it.sh
+++ install_it.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Note: This script must be run in virtual env, if it exists!
 # WARNING: Do not run this while running tensorflow, or you may run into bus errors!
 
-TF_PKG_DIR="/home/andrewor/tensorflow_pkg"
-module load anaconda3/5.2.0
-./bazel-bin/tensorflow/tools/pip_package/build_pip_package "$TF_PKG_DIR"
+source build_flags.sh
 
+if [[ "$ENVIRONMENT" == "tigergpu" ]]; then
+  module load anaconda3/5.2.0
+fi
+
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package "$TF_PKG_DIR"
 TF_PKG_NAME="$(ls $TF_PKG_DIR | grep .whl)"
 pip uninstall -y tensorflow tensorflow-gpu
 pip install --user "$TF_PKG_DIR/$TF_PKG_NAME"
